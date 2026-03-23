bcc9452980441968270f619328473dc06aa9a5f1
Jeffrey E Haupt <haupt2it@mac.com>
2026-03-23 14:40:37 -0500

    Initial Commit

---
+++ SunAndSky/SunAndSkyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunAndSkyApp.swift
+//  SunAndSky
+//
+//  Created by Jeffrey E Haupt on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct SunAndSkyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SunAndSkyTests/SunAndSkyTests.swift
@@ -0,0 +1,17 @@
+//
+//  SunAndSkyTests.swift
+//  SunAndSkyTests
+//
+//  Created by Jeffrey E Haupt on 3/23/26.
+//
+
+import Testing
+@testable import SunAndSky
+
+struct SunAndSkyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
